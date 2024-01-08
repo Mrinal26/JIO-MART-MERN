cffa7a0e65f531441fc6f8679ba851b4b8d03d5c
Mrinal26 <Mrinalkumar5046@gmail.com>
2024-01-08 18:10:01 +0530

    both done

---
+++ backend/Models/Electronics.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const ElectronicsSchema = mongoose.Schema({
+  image: String,
+  title: String,
+  price: Number,
+  userID: String,
+},{
+  versionKey:false
+});
+
+const ElectronicsModel = mongoose.model("Electronics", ElectronicsSchema);
+
+module.exports = { ElectronicsModel };
+
+// ₹
---
+++ backend/Models/Fashion.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const menSchema = mongoose.Schema({
+  image: String,
+  title: String,
+  price: Number,
+  userID: String,
+},{
+  versionKey:false
+});
+
+const menModel = mongoose.model("men", menSchema);
+
+module.exports = { menModel };
+
+// ₹
